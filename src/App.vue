f5e477d8e4e91df50651e8e4e681c02663b5e8aa
Erland Nafid Friskiawan <erland.nafid@gmail.com>
2019-11-02 03:03:16 +0700

    add posts

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ul>
+      <li v-for="(post, index) in posts" :key="index">{{ post.title }}</li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data() {
+    return {
+      posts: [],
+      // eslint-disable-next-line
     }
-  }
-}
-</style>
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        this.posts = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
