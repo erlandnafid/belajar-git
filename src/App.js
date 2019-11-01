bd77cd838c5e2adf5194dfaa9eefeea92a09cd8b
Erland Nafid Friskiawan <erland.nafid@gmail.com>
2019-11-02 04:25:34 +0700

    posto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        this.setState({ posts: res.data})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  render() {
+    const post = this.state.posts.map((post, index) => 
+      <li key={index}>{post.title}</li>
+    )
+
+    return (
+      <div className="App">
+        <h1>React</h1>
+        <ul>
+          { post }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
